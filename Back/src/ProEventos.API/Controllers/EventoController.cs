26f6a4565ee07d2904e690976e942c46e50f0115
Lucas <ldanezine@gmail.com>
2022-02-23 01:11:18 -0300

    Feito referencias do EF 5 e injeção de contexto. Criação de contexto e migrations

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ProEventos.API;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -13,48 +14,29 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public EventoController()
+        private readonly DataContext context;
+        public EventoController(DataContext context)
         {
-           
-        }
+            this.context = context;
 
-        public IEnumerable <Evento> _evento = new Evento[] { 
-                
-               new Evento(){
-               EventoId = 1,
-               Tema = "Angular 11 e .NET5",
-               Local = "BH",
-               Lote = "1º Lote",
-               QtdPessoas = 250,
-               DataEvento = DateTime.Now.AddDays(2).ToString()   
-               },
-                new Evento(){
-               EventoId = 2,
-               Tema = "Angular 11 e .NET5",
-               Local = "BH",
-               Lote = "1º Lote",
-               QtdPessoas = 250,
-               DataEvento = DateTime.Now.AddDays(2).ToString()   
-               }
-           
-        };
+        }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-           return _evento;
+            return context.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> Get(int id)
+        public Evento Get(int id)
         {
-           return _evento.Where(evento => evento.EventoId == id);
+            return context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
         }
 
         [HttpPut]
         public string Put(int id)
         {
-           return "value" + id;
+            return "value" + id;
         }
     }
 }
